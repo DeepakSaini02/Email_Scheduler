0d7a595a0c08e904bbc84554542108cfb7215b9a
Deepak Saini <91534605+DeepakSaini02@users.noreply.github.com>
2022-03-11 19:34:50 +0530

    add mail controller

--- src/app.js
+++ src/app.js
@@ -2,10 +2,11 @@ const express = require('express')
 
 const app = express()
 const connect = require('./configs/db')
+const mailController = require("./controllers/mail.controller")
 app.use(express.json())
 
 
-
+app.use('/sendmail', mailController)
 
 app.listen(6060, () => {
     connect()
